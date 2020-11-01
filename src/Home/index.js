e59e4adf4606182b09d98c5377d3f364ba24b41c
Vito Breda <vito_odoni@hotmail.com>
2020-11-01 14:47:35 -0300

    backup

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import {View} from 'react-native'
+import React from 'react';
+import { View, Button } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-export default function Home() {
-    return (
-        <View></View>
-    )
+export default function Home(props) {
+  function Logout() {
+    auth()
+      .signOut()
+      .then(() => console.log('user are logout'))
+      .catch((err) => console.log(err));
+
+    props.navigation.navigate('Login');
+  }
+
+  return <Button title="Logout" onPress={() => Logout()}></Button>;
 }
--- src/Login/index.js
+++ src/Login/index.js
@@ -3,106 +3,181 @@ import { Alert, Image } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import RaidsTextInput from '../Components/RaidsTextInput';
 import RaidsButtom from '../Components/RaidsButtom';
-import { LoginManager, AccessToken } from "react-native-fbsdk";
-import * as S from './styles'
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import * as S from './styles';
 
-export default function Login() {
+export default function Login(props) {
   const [user, setUser] = useState();
   const [password, setPassword] = useState();
   const [token, setToken] = useState();
 
   useEffect(() => {
     //auth().signOut().then(() => console.log('user are logout')).catch((err) => console.log(err))
-
     auth().onAuthStateChanged((authUser) => {
+      setUser(authUser);
       if (authUser) {
-        authUser.getIdToken()
-        .then((token) => { setToken(token) })
-        .catch((err) => {
-          Alert.alert(
-            "Falha ao solicitar o token",
-            err.message)
-        })
+        getTokenFirebase(authUser);
       }
     });
-  }, [])
+  }, []);
 
-  useEffect(() => {
-    console.log('manteve a sessão com o token: ', token)
-  }, [token])
 
-  function login() {
-    auth().face
 
-    auth().signInWithEmailAndPassword(user, password)
-      .then((success) => {
-        setUser(success.user)
-        success.user.getIdToken()
-          .then((token) => { setToken(token) })
-          .catch((err) => {
-            Alert.alert(
-              "Falha ao solicitar o token",
-              err.message)
-          })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ,
+  ,
+  11000000
+
+  function getTokenFirebase(authUser) {
+    authUser
+      .getIdToken()
+      .then((token) => {
+        setToken(token);
       })
       .catch((err) => {
-        switch (err.code) {
-          case 'auth/wrong-password':
-            Alert.alert(
-              "Falha no login",
-              "Usuario ou senha invalidos")
-            break;
-          case 'auth/user-not-found':
-            Alert.alert(
-              "Falha no login",
-              "Nenhum usuario encontrado esse endereço de email")
-            break;
-          default:
-            Alert.alert(
-              "Falha no login",
-              "Ocorreu um erro ao tentar fazer o login")
+        Alert.alert('Falha ao solicitar o token', err.message);
+      });
+  }
+
+  useEffect(() => {
+    props.navigation.navigate('Home');
+  }, [token]);
+
+  function login() {
+    auth()
+      .signInWithEmailAndPassword(user, password)
+      .then((success) => {
+        setUser(success.user);
+
+        if (success.user) {
+          getTokenFirebase(success.user);
         }
       })
+      .catch((err) => handleErrorLoginFirebase(err));
+  }
+
+  function handleErrorLoginFirebase(error) {
+    switch (error.code) {
+      case 'auth/wrong-password':
+        Alert.alert('Falha no login', 'Usuario ou senha invalidos');
+        break;
+      case 'auth/user-not-found':
+        Alert.alert(
+          'Falha no login',
+          'Nenhum usuario encontrado esse endereço de email',
+        );
+        break;
+      default:
+        Alert.alert(
+          'Falha no login',
+          'Ocorreu um erro ao tentar fazer o login',
+        );
+    }
   }
 
   function getFacebookToken() {
     AccessToken.getCurrentAccessToken()
-      .then(
-        (data) => {
-          console.log('facebook data: ', data)
-          if (data) {
-            const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+      .then((data) => {
+        if (data) {
+          const facebookCredential = auth.FacebookAuthProvider.credential(
+            data.accessToken,
+          );
 
-            // Sign-in the user with the credential
-            return auth().signInWithCredential(facebookCredential).then((value) => {console.log('firebase auth with facebook: ', value)}).catch((err) => {console.log(err)});
-            
-            console.log('facebook token', data.accessToken)
-            return data.accessToken;
-          }
-        })
-      .catch((err) => { console.log('ocorreu um erro ao tentar requisitar o token do facebook: ', err) })
+          // Sign-in the user with the credential
+          return auth()
+            .signInWithCredential(facebookCredential)
+            .then((value) => {
+              if (value.user) {
+                getTokenFirebase(value.user);
+              }
+            })
+            .catch((err) => handleErrorLoginFirebase(err));
+        }
+      })
+      .catch((err) => {
+        Alert.alert('Erro ao logar com o facebook', err.message);
+      });
   }
 
   async function onFacebookButtonPress() {
     // Attempt login with permissions
-    LoginManager.logInWithPermissions(["public_profile"]).then(
+    LoginManager.logInWithPermissions(['public_profile']).then(
       function (result) {
         if (result.isCancelled) {
-          console.log("Login cancelled");
+          console.log('Login cancelled');
         } else {
-          return getFacebookToken()
+          return getFacebookToken();
         }
       },
       function (error) {
-        console.log("Login fail with error: " + error);
-      }
+        console.log('Login fail with error: ' + error);
+      },
     );
   }
-
-  async function onFacebookLogout() {
-    LoginManager.logOut()
-  }
-
 
   return (
     <S.PageDefault>
@@ -111,9 +186,9 @@ export default function Login() {
           style={{
             width: 300,
             height: 150,
-            alignSelf: "center",
+            alignSelf: 'center',
             marginTop: '-10%',
-            marginBottom: '10%'
+            marginBottom: '10%',
           }}
           source={require('../Assets/Logo.png')}
         />
@@ -135,20 +210,22 @@ export default function Login() {
         </S.ComponentWrapper>
 
         <S.ComponentWrapper>
-          <RaidsButtom
-            textValue="Continuar"
-            onPres={() => login()}
-          />
+          <RaidsButtom textValue="Continuar" onPres={() => login()} />
         </S.ComponentWrapper>
 
         <S.ComponentWrapper>
           <S.ContainerFacebookButtom
-            onPress={() => onFacebookButtonPress().then(() => console.log('Signed in with Facebook!'))}
+            onPress={() =>
+              onFacebookButtonPress().then(() =>
+                console.log('Signed in with Facebook!'),
+              )
+            }
           >
-            <S.TextFacebookButtom>Logar com o Facebook</S.TextFacebookButtom>
+            <S.TextFacebookButtom>
+              Logar com o Facebook
+            </S.TextFacebookButtom>
           </S.ContainerFacebookButtom>
         </S.ComponentWrapper>
-
       </S.WrapperContent>
     </S.PageDefault>
   );
